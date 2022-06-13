3d6ed6f52f16401bf5592637f205fcaee5841a59
Prisca C <prisca.castanier@gmail.com>
2022-06-14 01:50:07 +0200

    Changed List name

---
+++ src/Stories.js
@@ -0,0 +1,31 @@
+const Stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+      date: 2011
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+      date: 2015
+    },
+    {
+      title: 'VueJS',
+      url: 'https://vuejs.org/',
+      author: 'Evan You',
+      num_comments: 2,
+      points: 6,
+      objectID: 2,
+      date: 2014
+    }
+  ];
+
+  export default Stories;
