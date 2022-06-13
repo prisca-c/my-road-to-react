b603a29892556484fbc677ed5111c53cd0df8cab
Prisca C <prisca.castanier@gmail.com>
2022-06-13 17:56:40 +0200

    Create Component - Learn Component Tree"

--- src/App.js
+++ src/App.js
@@ -1,39 +1,57 @@
 import React from 'react';
 import './App.css';
-import Customers from './Customers';
+import TableList from './TableList.js';
 
-
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
   
 function App() {
   return (
     <div>
+
       <h1>My Hacker Stories</h1>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <SearchInput />
       <hr />
-      {/* render the list here */}
+
+    {TableList.map( item => {
+      if(item.date > 2013) 
+      {
+      return <List key={item.objectID}  
+        author={item.author}
+        num_comments={item.num_comments}
+        points={item.points}
+        date={item.date} />
+      } else { 
+        return null 
+      }
+    })}
+
     </div>
   );
 }
+
+function List (props) {
   
+        return(
+          <div>
+            <span>
+              <a href='{props.url}'>{props.title}</a>
+            </span>
+            <span> Author: {props.author}</span>
+            <span> Comments: {props.num_comments}</span>
+            <span> Points: {props.points}</span>
+            <span> Date: {props.date}</span>
+          </div>);
+      
+    }
+  ;
+
+function SearchInput () {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
 
 
 
