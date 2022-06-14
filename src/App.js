870389752dbb8c6ef20e6cac39682ff95b100b21
Prisca C <prisca.castanier@gmail.com>
2022-06-15 01:13:50 +0200

    Spread & Rest Operators

--- src/App.js
+++ src/App.js
@@ -19,30 +19,34 @@ const App = () => {
 
   return (
     <div>
+      
       <h1>My Hacker Stories</h1>
 
       <SearchInput search={searchTerm} onSearch={handleSearch} />
       <hr />
 
       <List list={searchedStories} />
+
     </div>
   );
 }
 
 const List = ({ list }) =>
-  list.map(item => 
-    <Item key={item.objectID} item={item} />
-);
+  list.map(({ objectID, ...item }) => ( 
+    <Item key={objectID} {...item} />
+));
     
-const Item = ({ item }) => (
-    <div key={item.objectID}>
+const Item = ({ title, author, num_comments, points, url, date }) => (
+    <div>
+
       <span>
-        <a href={item.url}>{item.title}</a>
+        <a href={url}>{title}</a>
       </span>
-      <span> Author: {item.author}</span>
-      <span> Comments: {item.num_comments}</span>
-      <span> Points: {item.points}</span>
-      <span> Date: {item.date}</span>
+      <span> Author: {author}</span>
+      <span> Comments: {num_comments}</span>
+      <span> Points: {points}</span>
+      <span> Date: {date}</span>
+
     </div>
 );
 
@@ -50,6 +54,7 @@ const SearchInput = ({ search, onSearch }) => {
 
   return (
     <div>
+
       <label htmlFor="search">Search: </label>
       <input 
         id="search" 
